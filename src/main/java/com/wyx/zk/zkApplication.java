938036e1aa50859c29e9d9c34e55268cd0bea0ab
MrWangYongxin <jsntwyx@sina.com>
2021-01-19 15:19:44 +0800

    初始化项目

---
+++ src/main/java/com/wyx/zk/zkApplication.java
@@ -0,0 +1,13 @@
+package com.wyx.zk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class zkApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(zkApplication.class, args);
+  }
+
+}
